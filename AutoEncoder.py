10a65ce655cfbf71b60325519764b2410d25ef17
zts <hquzts@163.com>
2017-04-22 14:42:37 +0800

    3

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -55,3 +55,32 @@ class Autoencoder(object):
 
     def getBiases(self):
         return self.sess.run(self.weights['b1'])
+
+mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
+
+def standar_scale(X_train,X_test):
+    preprocessor = prep.StandardScaler().fit(X_train)
+    X_train = preprocessor.transform(X_train)
+    X_test = preprocessor.transform(X_test)
+    return X_train,X_test
+
+def get_random_block_from_data(data,batch_size):
+    start_index = np.random.randint(0,len(data)-batch_size)
+    return data[start_index:(start_index+batch_size)]
+
+X_train,X_test = standar_scale(mnist.train.images,mnist.test.images)
+
+n_samples = int(mnist.train.num_example)
+train_epochs = 20
+batch_size = 128
+display_step = 1
+
+autoencoder = Autoencoder(n_input=784,n_hidden=200,transfer_function=tf.nn.softplus,
+                          optimizer=tf.train.AdamOptimizer(learning_rate=0.001),scale=0.1)
+
+for epoch in range(train_epochs):
+    avg_cost = 0
+    total_batch = int(n_samples/batch_size)
+    for i in range(total_batch):
+        batch_xs = get_random_block_from_data(X_train,batch_size)
+        cost = autoencoder.partial_fit(batch_size)
